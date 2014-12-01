b4e7a3ca2af4f1ea737fc07ef59c9bb3a6ac5aa5
markhughneri <mark@oxygenventures.com.au>
2014-12-01 13:38:47 +0800

    changed disqus shortname

--- src/Ticy.Web/Content/disqus/disqus.js
+++ src/Ticy.Web/Content/disqus/disqus.js
@@ -1,5 +1,5 @@
 ﻿/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var disqus_shortname = 'test-ticy'; // required: replace example with your forum shortname
+var disqus_shortname = 'ticyorg'; // required: replace example with your forum shortname
 
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function () {
